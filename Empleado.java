160833ac8433576cbcb9017cdf3373bdd626d418
Zaher <zaherpersonal@email.com>
2025-05-26 10:33:17 +0200

    Centralizados todos los textos fijos en la clase Textos para facilitar la corrección y traducción de mensajes en pantalla.

--- Empleado.java
+++ Empleado.java
@@ -1,7 +1,7 @@
 class Empleado {
-    String nombre;
-    String cargo;
-    double salario;
+    private String nombre;
+    private String cargo;
+    private double salario;
 
     public Empleado(String nombre, String cargo, double salario) {
         this.nombre = nombre;
@@ -9,12 +9,38 @@ class Empleado {
         this.salario = salario;
     }
 
-    @Override
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+   @Override
     public String toString() {
-        return "Empleado{" +
-                "nombre='" + nombre + '\'' +
-                ", cargo='" + cargo + '\'' +
-                ", salario=" + salario +
-                '}';
+        return Textos.EMPLEADO_PREFIX +
+                Textos.NOMBRE + nombre + '\'' +
+                Textos.CARGO + cargo + '\'' +
+                Textos.SALARIO + salario +
+                Textos.SUFFIX;
     }
 }
---
+++ Textos.java
@@ -0,0 +1,8 @@
+public class Textos {
+    public static final String EMPLEADO_PREFIX = "Empleado{";
+    public static final String NOMBRE = "nombre='";
+    public static final String CARGO = ", cargo='";
+    public static final String SALARIO = ", salario=";
+    public static final String SUFFIX = "'}";
+    public static final String TRAS_AUMENTO = "\nTras aumento de salario:";
+}
